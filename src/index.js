22c18c64c7a0f6203f54fd1cdc81b74fa9046b73
Juan Gomez <juangomez1947@outlook.com>
2022-01-27 16:53:58 -0500

    Se cambia estructturación de carpetas

--- src/index.js
+++ src/index.js
@@ -1,16 +1,6 @@
 import fetch from "node-fetch";
 import "dotenv/config";
 
- /*function getDateFromAspNetFormat( date ) {
-    const re = /-?\d+/;
-    const m = re.exec(date);
-    return parseInt(m[0], 10);
-}
-
-
-let myDate = new Date(getDateFromAspNetFormat('/Date(1643172900470-0500)/'));
-
-console.log( new Date(  ) );*/
 const updateTestCaseTestRun =  async ( inputDate ) => {
     const endpointGetTestSteps = "https://alm-itac.spiraservice.net/services/v5_0/RestService.svc/projects/" + inputDate.idProject +"/test-cases/" + inputDate.idTestCase + 
     "/test-steps?username=" + process.env.USER_NAME + "&api-key={" + process.env.API_KEY + "}";
@@ -22,12 +12,6 @@ const updateTestCaseTestRun =  async ( inputDate ) => {
     });
     return responseBodyTestSteps.json(  );
 };
-const request = {
-    idProject: 37,
-    idRelease: 1880,
-    idTestCase: 29619,
-    idTestSet: 1621
-}
 
 function callFunctionUpdateTestRun( inputDate, countCasesFailed = 0 ) {
     try {
@@ -79,4 +63,9 @@ function callFunctionUpdateTestRun( inputDate, countCasesFailed = 0 ) {
     } finally {
         console.log( "Proceso de validación del TestCase finalizado" );
     }
+}
+
+module.exports = {
+    updateTestCaseTestRun,
+    callFunctionUpdateTestRun
 }
