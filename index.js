33882137f9d65b35891b347acfa04f58adf16b18
Juan Gomez <juangomez1947@outlook.com>
2022-01-27 15:50:48 -0500

    Actualización para el llamado de la promesa desde una función

--- index.js
+++ index.js
@@ -11,83 +11,70 @@ import "dotenv/config";
 let myDate = new Date(getDateFromAspNetFormat('/Date(1643172900470-0500)/'));
 
 console.log( new Date(  ) );*/
-
-console.log( process.env.API_KEY );
-
-function getStepsTestCases(  ) { 
-
-}
-
-
-//Creamos una promesa para la función addTestRun
-async function addTestRun( request, countStepsFailed = 0 ) {
-    const endpointService = "https://alm-itac.spiraservice.net/services/v5_0/RestService.svc/projects/37/test-runs?end_date=&username=" + process.env.USER_NAME + 
-    "&api-key={" + process.env.API_KEY + "}";
-    
-    //Llamamos a la función fecth de la dependecia node_fetch. En el primer argumento se le pasa la url del servicio que se va a consumir, y el segundo, un objeto con las caracteristicas del
-    //servicio.
-    const response = await fetch( endpointService, {
-        method: "PUT",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify( [{
-            "ArtifactTypeId": 5,
-            "ConcurrencyDate": "/Date(1643171723693-0500)/",
-            "CustomProperties": [],
-            "IsAttachments": false,
-            "ProjectId": request.idProject,
-            "ActualDuration": 0,
-            "BuildId": null,
-            "EndDate": "/Date(1643179787797-0500)/",
-            "EstimatedDuration": null,
-            "ExecutionStatusId": 2,
-            "Name": "test case automatizacion prueba - 26",
-            "ReleaseId": request.idRelease,
-            "StartDate": "/Date(1643171714553-0500)/",
-            "TestCaseId": request.idTestCase,
-            "TestRunTypeId": 1,
-            "TestSetId": request.idTestSet,
-            "TestSetTestCaseId": null,
-            "TesterId": 43,
-            "TestRunSteps": [
-                {
-                    "ActualDuration": 0,
-                    "ActualResult": "<p>d22f</p>",
-                    "Description": "Description",
-                    "EndDate": "/Date(1643179787797-0500)/",
-                    "ExecutionStatusId": 2,
-                    "ExpectedResult": "Works as expected.",
-                    "Position": 1,
-                    "SampleData": null,
-                    "StartDate": "/Date(1643171714553-0500)/",
-                    "TestCaseId": 29619,
-                    "TestStepId": 169427
+const updateTestCaseTestRun =  async ( inputDate ) => {
+    try {
+        if( !inputDate.idTestCase ) {
+            throw "El parámetro idTestCase es obligatorio.";
+        } else if( !inputDate.idProject ) {
+            throw "El parámetro idProject es obligatorio";
+        } else {
+            const endpointGetTestSteps = "https://alm-itac.spiraservice.net/services/v5_0/RestService.svc/projects/" + inputDate.idProject +"/test-cases/" + inputDate.idTestCase + 
+            "/test-steps?username=" + process.env.USER_NAME + "&api-key={" + process.env.API_KEY + "}";
+            const responseBodyTestSteps =  await fetch( endpointGetTestSteps, {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json"
                 }
-            ]
-        } ])
+            });
+            return responseBodyTestSteps.json(  );
+        }
+    } catch( error ) {
+        console.log( "Error: " + error );
+    } finally {
+        console.log( "Proceso de ejecución del TestCase finalizado" );
+    }
+};
+
+function callFunctionUpdateTestRun( inputDate ) {
+    updateTestCaseTestRun( inputDate ).then( async ( message ) => {
+        const endpointUpdateTestRun = "https://alm-itac.spiraservice.net/services/v5_0/RestService.svc/projects/37/test-runs?end_date=&username=" + process.env.USER_NAME + 
+        "&api-key={" + process.env.API_KEY + "}";
+        console.log( await fetch( endpointUpdateTestRun, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify( [{
+                "ArtifactTypeId": 5,
+                "ConcurrencyDate": "/Date(1643171723693-0500)/",
+                "CustomProperties": [],
+                "IsAttachments": false,
+                "ProjectId": inputDate.idProject,
+                "ActualDuration": 0,
+                "BuildId": null,
+                "EndDate": "/Date(1643179787797-0500)/",
+                "EstimatedDuration": null,
+                "ExecutionStatusId": 2,
+                "Name": "test case automatizacion prueba - 100",
+                "ReleaseId": inputDate.idRelease,
+                "StartDate": "/Date(1643171714553-0500)/",
+                "TestCaseId": inputDate.idTestCase,
+                "TestRunTypeId": 1,
+                "TestSetId": inputDate.idTestSet,
+                "TestSetTestCaseId": null,
+                "TesterId": 43,
+                "TestRunSteps": message
+            } ])
+        } ))
+        console.log( message[0].ArtifactTypeId );
     } );
-    return response.json(  );
 }
 
 const request = {
     idProject: 37,
     idRelease: 1880,
-    idTestCase: 29619,
+    idTestCase: undefined,
     idTestSet: 1621
 }
 
-//Se le pasa como argumento una función callback, en donde se realiza el llamdo de la promesa addTestRun (este se va a ejcutar en un intervalo de 5 seg.)
-setTimeout( (  ) => {
-    try {
-        addTestRun( request ).then( ( msg ) => { console.log( msg ); })
-    } catch ( e ) {
-        console.log( e );
-    }
-}, 5000 );
-
-
-
-
-
-
+callFunctionUpdateTestRun( request );
