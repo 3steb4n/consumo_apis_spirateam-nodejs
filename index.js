72b7154097e1da558f831123f70587e804afdd40
Juan Gomez <juangomez1947@outlook.com>
2022-01-27 16:26:29 -0500

    Se finaliza el desarrollo. Publicación V1.0.0

--- index.js
+++ index.js
@@ -12,69 +12,71 @@ let myDate = new Date(getDateFromAspNetFormat('/Date(1643172900470-0500)/'));
 
 console.log( new Date(  ) );*/
 const updateTestCaseTestRun =  async ( inputDate ) => {
+    const endpointGetTestSteps = "https://alm-itac.spiraservice.net/services/v5_0/RestService.svc/projects/" + inputDate.idProject +"/test-cases/" + inputDate.idTestCase + 
+    "/test-steps?username=" + process.env.USER_NAME + "&api-key={" + process.env.API_KEY + "}";
+    const responseBodyTestSteps =  await fetch( endpointGetTestSteps, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json"
+        }
+    });
+    return responseBodyTestSteps.json(  );
+};
+const request = {
+    idProject: 37,
+    idRelease: 1880,
+    idTestCase: 29619,
+    idTestSet: 1621
+}
+
+function callFunctionUpdateTestRun( inputDate, countCasesFailed = 0 ) {
     try {
-        if( !inputDate.idTestCase ) {
-            throw "El parámetro idTestCase es obligatorio.";
-        } else if( !inputDate.idProject ) {
-            throw "El parámetro idProject es obligatorio";
+        if( !inputDate.idProject ) {
+            throw "El parámetro idProject es requerido.";
+        } else if( !inputDate.idRelease ) {
+            throw "El parámetro idRelease es requerido.";
+        } else if( !inputDate.idTestCase ) {
+            throw "El parámetro idTestCase es requerido.";
+        } else if( !inputDate.idTestSet ) {
+            throw "El parámetro idTestSet es requerido.";
         } else {
-            const endpointGetTestSteps = "https://alm-itac.spiraservice.net/services/v5_0/RestService.svc/projects/" + inputDate.idProject +"/test-cases/" + inputDate.idTestCase + 
-            "/test-steps?username=" + process.env.USER_NAME + "&api-key={" + process.env.API_KEY + "}";
-            const responseBodyTestSteps =  await fetch( endpointGetTestSteps, {
-                method: "GET",
-                headers: {
-                    "Content-Type": "application/json"
-                }
-            });
-            return responseBodyTestSteps.json(  );
+            updateTestCaseTestRun( inputDate ).then( async ( message ) => {
+                ( countCasesFailed > 0 ) ? message[0].ExecutionStatusId = 1 : message[0].ExecutionStatusId = 2;
+                const endpointUpdateTestRun = "https://alm-itac.spiraservice.net/services/v5_0/RestService.svc/projects/37/test-runs?end_date=&username=" + process.env.USER_NAME + 
+                "&api-key={" + process.env.API_KEY + "}";
+                console.log( await fetch( endpointUpdateTestRun, {
+                    method: "PUT",
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify( [{
+                        "ArtifactTypeId": 5,
+                        "ConcurrencyDate": "/Date(1643171723693-0500)/",
+                        "CustomProperties": [],
+                        "IsAttachments": false,
+                        "ProjectId": inputDate.idProject,
+                        "ActualDuration": 0,
+                        "BuildId": null,
+                        "EndDate": "/Date(1643179787797-0500)/",
+                        "EstimatedDuration": null,
+                        "ExecutionStatusId": ( countCasesFailed > 0 ) ? 1 : 2,
+                        "Name": "test case automatizacion prueba - 200",
+                        "ReleaseId": inputDate.idRelease,
+                        "StartDate": "/Date(1643171714553-0500)/",
+                        "TestCaseId": inputDate.idTestCase,
+                        "TestRunTypeId": 1,
+                        "TestSetId": inputDate.idTestSet,
+                        "TestSetTestCaseId": null,
+                        "TesterId": 43,
+                        "TestRunSteps": message
+                    } ])
+                } ))
+                console.log( message[0] );
+            } );
         }
     } catch( error ) {
         console.log( "Error: " + error );
     } finally {
-        console.log( "Proceso de ejecución del TestCase finalizado" );
+        console.log( "Proceso de validación del TestCase finalizado" );
     }
-};
-
-function callFunctionUpdateTestRun( inputDate ) {
-    updateTestCaseTestRun( inputDate ).then( async ( message ) => {
-        const endpointUpdateTestRun = "https://alm-itac.spiraservice.net/services/v5_0/RestService.svc/projects/37/test-runs?end_date=&username=" + process.env.USER_NAME + 
-        "&api-key={" + process.env.API_KEY + "}";
-        console.log( await fetch( endpointUpdateTestRun, {
-            method: "PUT",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify( [{
-                "ArtifactTypeId": 5,
-                "ConcurrencyDate": "/Date(1643171723693-0500)/",
-                "CustomProperties": [],
-                "IsAttachments": false,
-                "ProjectId": inputDate.idProject,
-                "ActualDuration": 0,
-                "BuildId": null,
-                "EndDate": "/Date(1643179787797-0500)/",
-                "EstimatedDuration": null,
-                "ExecutionStatusId": 2,
-                "Name": "test case automatizacion prueba - 100",
-                "ReleaseId": inputDate.idRelease,
-                "StartDate": "/Date(1643171714553-0500)/",
-                "TestCaseId": inputDate.idTestCase,
-                "TestRunTypeId": 1,
-                "TestSetId": inputDate.idTestSet,
-                "TestSetTestCaseId": null,
-                "TesterId": 43,
-                "TestRunSteps": message
-            } ])
-        } ))
-        console.log( message[0].ArtifactTypeId );
-    } );
 }
-
-const request = {
-    idProject: 37,
-    idRelease: 1880,
-    idTestCase: undefined,
-    idTestSet: 1621
-}
-
-callFunctionUpdateTestRun( request );
